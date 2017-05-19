429b7bae25d55bc8f0e96c431861cfd043da9c87
Jens Finkhaeuser <jens@finkhaeuser.de>
2017-05-19 19:15:48 +0200

    Flake8

--- cereal/__init__.py
+++ cereal/__init__.py
@@ -17,58 +17,123 @@ __version__ = '0.1.0'
 
 
 class DERTranscoder(object):
-  def __init__(self, inner):
-    self.inner = inner
+  """
+  DER-encode Python builtin (and extended) types.
+
+  The class first uses ASN1Transcoder to encode valuesin ASN.1 classes, then
+  DER-encodes the result.
+  """
+
+  def __init__(self, inner = None):
+    """
+    Initialize DERTranscoder.
+
+    The sole parameter is a reference to an inner decoder. If not specified,
+    this defaults to an ASN1Transcoder instance with default options.
+
+    :param object inner: [optional] Inner transcoder
+    """
+    self.inner = inner or ASN1Transcoder()
 
   def encode(self, value):
+    """
+    DER-encode the given value.
+
+    Nested values are encoded recursively.
+
+    :param mixed value: The value to encode.
+    :return: An DER-encoded ASN.1 value, i.e. a byte sequence.
+    """
     from pyasn1.codec.der import encoder
     return encoder.encode(self.inner.encode(value))
 
   def decode(self, value):
+    """
+    DER-deocde the given byte sequence.
+
+    :param bytes value: The value to decode.
+    :return: A Python value, the result of passing the parameter through DER
+        decoding and ASN.1 decoding.
+    """
     from pyasn1.codec.der import decoder
     decoded = decoder.decode(value)
     return self.inner.decode(decoded[0])
 
 
 class ASN1Transcoder(object):
+  """
+  Transcode Python builtin (and extended) types to and from ASN.1 classes.
+
+  Note that encoding is lossy with regards to specialized types.
+
+  For many Python builtin types, there are corresponding universal ASN.1 data
+  types available.
+
+  For tuple() and list(), we use explicit tagging (which, when DER-encoded,
+  takes up a few extra Bytes of space) to disambiguate one kind of sequence
+  from another. Any collections.Sequence that is not specifically a list is
+  encoded as a tuple(), any additional type information.
+
+  Similarly, any collections.Mapping is encoded in the same way, and so is
+  any collections.Set. If you use special subtypes of these Abstract Base
+  Classes, that type information is lost.
+
+  Still, it means that any nested structure will result in an equality-
+  comparable nested structure, which is all that often times is required.
+  """
 
   from pyasn1.type import univ, tag, namedtype
 
-  #: complex
-  COMPLEX   = univ.Sequence(
-                tagSet = univ.Sequence.tagSet.tagExplicitly(
-                  tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x01)
-                )
-              )
-  #: collections.Sequence
-  TUPLE     = univ.Sequence(
-                tagSet = univ.Sequence.tagSet.tagExplicitly(
-                  tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x02)
-                )
-              )
-  LIST      = univ.Sequence(
-                tagSet = univ.Sequence.tagSet.tagExplicitly(
-                  tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x03)
-                )
-              )
-  #: collections.Mapping
-  MAPPING   = univ.Sequence(
-                tagSet = univ.Sequence.tagSet.tagExplicitly(
-                  tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x04)
-                )
-              )
-  #: collections.Set
-  SET       = univ.Set(
-                tagSet = univ.Set.tagSet.tagExplicitly(
-                  tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x05)
-                )
-              )
-
+  #: Tags for complex
+  COMPLEX = univ.Sequence(
+    tagSet = univ.Sequence.tagSet.tagExplicitly(
+      tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x01)
+    )
+  )
+  #: Tags for collections.Sequence
+  TUPLE = univ.Sequence(
+    tagSet = univ.Sequence.tagSet.tagExplicitly(
+      tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x02)
+    )
+  )
+  LIST = univ.Sequence(
+    tagSet = univ.Sequence.tagSet.tagExplicitly(
+      tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x03)
+    )
+  )
+  #: Tags for collections.Mapping
+  MAPPING = univ.Sequence(
+    tagSet = univ.Sequence.tagSet.tagExplicitly(
+      tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x04)
+    )
+  )
+  #: Tags for collections.Set
+  SET = univ.Set(
+    tagSet = univ.Set.tagSet.tagExplicitly(
+      tag.Tag(tag.tagClassContext, tag.tagFormatConstructed, 0x05)
+    )
+  )
 
   def __init__(self, **kwargs):
+    """
+    Initialize the transcoder.
+
+    Keyword arguments are stored as options. Some such recognized options are:
+
+    :param bool sort: If True, encodes collections.Mappings in sorted key
+        order (using the sorted() function). The default is True.
+    """
     self.options = kwargs
 
   def encode(self, value):
+    """
+    Encode the given Python value.
+
+    Nested values (for Sequence, Set and Mapping) are recursively encoded.
+
+    :param mixed value: The value to encode.
+    :return: An ASN.1 class encapsulating the value.
+    """
     import six, collections
     from pyasn1.type import univ, char
 
@@ -108,29 +173,25 @@ class ASN1Transcoder(object):
     else:
       return self.__encode_from_registry(value)
 
-
   def __encode_from_registry(self, value):
     # TODO type registry
     raise TypeError('Cannot encode value of type "%s"!' % (type(value),))
 
-
   def __encode_complex(self, value):
-    from pyasn1.type import univ, tag
-
-    val = self.get_base(ASN1Transcoder.COMPLEX).clone()
+    # Encode complex() values
+    val = ASN1Transcoder.COMPLEX.clone()
 
     val.setComponentByPosition(0, self.encode(value.real))
     val.setComponentByPosition(1, self.encode(value.imag))
 
     return val
 
-
   def __encode_sequence(self, value):
     # Everything except for lists get coerced to a tuple
     base = ASN1Transcoder.TUPLE
     if isinstance(value, list):
       base = ASN1Transcoder.LIST
-    val = self.get_base(base).clone()
+    val = base.clone()
 
     # Sequences can't be re-ordered
     for idx, item in enumerate(value):
@@ -138,10 +199,9 @@ class ASN1Transcoder(object):
 
     return val
 
-
   def __encode_mapping(self, value):
     # coerce to dict
-    val = self.get_base(ASN1Transcoder.MAPPING).clone()
+    val = ASN1Transcoder.MAPPING.clone()
 
     # Force ordering of keys by default
     keys = value.keys()
@@ -153,10 +213,9 @@ class ASN1Transcoder(object):
 
     return val
 
-
   def __encode_set(self, value):
     # Everything is a set
-    val = self.get_base(ASN1Transcoder.SET).clone()
+    val = ASN1Transcoder.SET.clone()
 
     # Force ordering of items by default
     items = value
@@ -168,8 +227,15 @@ class ASN1Transcoder(object):
 
     return val
 
-
   def decode(self, value):
+    """
+    Decode the given ASN.1 class into a Python value.
+
+    Nested values (for Sequence, Set and Mapping) are recursively decoded.
+
+    :param mixed value: The value to decode.
+    :return: An Python value.
+    """
     # import six, collections
     from pyasn1.type import univ, char
 
@@ -221,7 +287,11 @@ class ASN1Transcoder(object):
         ret.add(item)
       return ret
 
+    else:
+      raise TypeError("Can't decode!")  # FIXME registry
+
   def __sequence_iter(self, seq):
+    # Iterate through a univ.Sequence, decode and yield each item
     idx = 0
     while True:
       try:
@@ -230,10 +300,3 @@ class ASN1Transcoder(object):
         idx += 1
       except IndexError:
         break
-
-
-  def get_base(self, base):
-    # TODO
-    return base
-    # bases = getattr(self.options, 'bases', {})
-    # return bases.get(base, base)
