375f1897767c565aa81548cc71dce0aa534632a8
Jens Finkhaeuser <jens@finkhaeuser.de>
2017-09-28 08:36:38 +0200

    Bump version: 0.2.0 → 0.3.0

--- bran/__init__.py
+++ bran/__init__.py
@@ -13,7 +13,7 @@ __author__ = 'Jens Finkhaeuser'
 __copyright__ = 'Copyright (c) 2017 Jens Finkhaeuser'
 __license__ = 'MIT +no-false-attribs'
 __all__ = ('hash')
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 class DERTranscoder(object):
